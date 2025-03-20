1be974092da8dfb0945bea06bd78b984685f3d62
LestherLopez <3010308470101@ingenieria.usac.edu.gt>
2025-03-20 15:48:30 -0600

    lectura csv en /clase9

--- clase9/src/clase9/Clase9.java
+++ clase9/src/clase9/Clase9.java
@@ -2,36 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package chart;
-import Model.DatosGrafico;
-import View.Inicio;
+package clase9;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+
 /**
  *
  * @author Lesther
  */
-public class Chart {
+public class Clase9 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] categorias = {"Enero", "Febrero", "Marzo", "Abril"};
-        int[] valores = {60, 25, 100, 200};
-
-        // Crear el modelo de datos
-        DatosGrafico datos = new DatosGrafico(categorias, valores);
-
-        // Crear la vista y mostrar el gráfico por medio del metodo mostrarGrafico que recibe como parametro los datis
-        Inicio inicio = new Inicio();
-        inicio.mostrarGrafico(datos);
-
-        // Mostrar la ventana
-        inicio.setVisible(true);
+        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingresa la ruta de tu archivo: ");
         String nombreArchivo = scanner.nextLine();
@@ -41,8 +30,8 @@ public class Chart {
             return;
         }
         leerCSV(nombreArchivo);
-        
     }
+    
     public static void leerCSV(String nombreArchivo){
         try(BufferedReader lector  = new BufferedReader(new FileReader(nombreArchivo))){
             String fila;
@@ -55,7 +44,6 @@ public class Chart {
                if (datos.length < 2){
                     continue;
                 } 
-                
                 String nombreJuego = datos[0];
                 int cantidad = Integer.parseInt(datos[1]);
                 System.out.println("juego: " + nombreJuego + " Cantidad: "+ cantidad);
@@ -66,4 +54,5 @@ public class Chart {
             System.out.println("Error al leer el archivo");
         }
     }
+    
 }
